fa98e8d9e600a7e8270568d6e5023426456d7a14
Alessio Greggi <ale_grey_91@hotmail.it>
2026-01-05 13:33:29 +0100

    style: add comments to classifier script
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- train_cve_multilabel_classifier.py
+++ train_cve_multilabel_classifier.py
@@ -106,8 +106,8 @@ def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
-        test_size=0.2,
-        random_state=42,
+        test_size=0.2, # use 20% of dataset for tests
+        random_state=42, # seed number to replicate results
     )
 
     pipeline.fit(X_train, y_train)
